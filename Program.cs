58219bc682a30cc87f709ccc093c146149364a01
Lars-Kristian <lakrsv@users.noreply.github.com>
2015-10-14 17:15:03 +0200

    Added description

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using OnlineReminders.App_Start;
+//A simple example mailing service that checks if it should send mail from an SQL database every X seconds based on a delay
+//specified in Program.cs.
+
+//It contains detailed step-by-step comments on what the program actually does, and how it works.
+
+//What was it used for?<
+
+//I used this simple little code to send mail from a website I made. 
+//This website let's you register with an e-mail address and then lets you create entries into a database. 
+//The fields you can submit are a Message and a Send Time.
+
+//So the website functions like a basic little reminder's website, 
+//you write a message and tell the website when to send it to you. 
+//This mailservice, running seperately, 
+//connects to the database at repeating intervals to check for messages and checks if the message should be sent. 
+//If these conditions are met, it sends the message and deletes the entry from the database.
+
+using OnlineReminders.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
